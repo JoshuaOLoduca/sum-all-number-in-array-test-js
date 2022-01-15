1c34b0dd1d09500294736118397392cc7ff4299b
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-01-15 00:12:22 +0000

    test

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+const sumItems = function(array) {
+  array.unshift(0);
+
+  const originalLength = array.length - 1;
+  let endOfArray = originalLength;
+
+  prepEndOfArr();
+  
+  for (let i = 0; i < originalLength; i ++) {
+    endOfArray = array.length - 1;
+
+    prepEndOfArr();
+    array[0] += Number(array.pop());
+  }
+
+  //return int
+  return array[0];
+
+  //hoisted above during runtime
+  function prepEndOfArr() {
+    if (Array.isArray(array[endOfArray])) {
+      array[endOfArray] = sumItems(array[endOfArray]);
+    }
+  }
+};
 
 module.exports = sumItems;
